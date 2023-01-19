7086fed56ac6a656323e2511be199ea821bbf46f
lzydavid <nodotanolife@hotmail.com>
2023-01-19 14:57:04 +0800

    FileOutputStream

--- src/main/java/sdf/App.java
+++ src/main/java/sdf/App.java
@@ -1,5 +1,10 @@
 package sdf;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  * Hello world!
  */
@@ -10,23 +15,52 @@ public final class App {
     /**
      * Says hello to the world.
      * @param args The arguments of the program.
+     * @throws IOException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
-        // CollectionSort cs = new CollectionSort();
-        // cs.example01();
+        String dirPath = "data";
+        String fileName = "data.txt";
 
-        // HashMapExample h = new HashMapExample();
-        // h.example01();
+        //create a directory
+        File newDir = new File(dirPath);
+        boolean  isDirCreated = newDir.mkdir();
 
-        // ConcurrentHashMapExample che = new ConcurrentHashMapExample();
-        // che.example();
+        if(isDirCreated){
+            System.out.println("New directory " + dirPath + " created");
+        }
+        else{
+            System.out.println("Directory " + dirPath + " already exist");
+        }
 
-        // LinkedListExample l = new LinkedListExample();
-        // l.example01();
+        //create a file in the directory created above
+        File newFile = new File(dirPath + File.separator + fileName);
+        boolean isFileCreated = newFile.createNewFile();
 
-        StackExample s = new StackExample();
-        s.example();
+        if(isFileCreated){
+            System.out.println("New File " + fileName + " created");
+        }
+        else{
+            System.out.println("File " + fileName + " already exist");
+        }
 
+        File fileList[] = newDir.listFiles();
+        for (File f:fileList){
+            System.out.println("File " + f.getPath() + ":" + f.getCanonicalPath());
+        }
+
+        FileOutputStream fos = new FileOutputStream(newFile,true);
+        for(int i=0;i<10;i++){
+            char str[] = Integer.toString(i).toCharArray();
+            fos.write(str[0]);
+            fos.write('\n');
+        }
+
+        String welcomeMessage = "Welcome to ISS TFIP";
+        byte[] outputData = welcomeMessage.getBytes();
+        fos.write(outputData);
+
+        fos.flush();
+        fos.close();
     }
 }
